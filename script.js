c3bb580b81e0240a838de3d2224c9ede5873d5b2
Fábio Bronze <fabio.r.bronze@hotmail.com>
2023-01-15 13:31:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,63 @@
-//Função que adiciona tarefa
+// Função que adiciona tarefa
 function addTask() {
-  //Titulo da tarefa
+  // Título da tarefa
   const taskTitle = document.querySelector("#task-title").value;
 
+  // Verifica se o título está ou não vazio, se estiver, não envia
   if (taskTitle) {
-    //clonar Template
+    // Clonar template em uma nova variável
     const template = document.querySelector(".template");
-    const newTask = template.cloneNode(true);
 
-    //adiciona titulo
+    // Nova tarefa
+    const newTask = template.cloneNode(true); // Retorna um clone do elemento para o qual foi invocado.
+
+    // Adiciona título
     newTask.querySelector("#task-title").textContent = taskTitle;
 
-    //remover classes desnecessárias
+    // Remover as classes desnecessárias
     newTask.classList.remove("template");
     newTask.classList.remove("hide");
 
-    //adiciona tarefa na lista
+    // Adiciona tarefa na lista
     const list = document.querySelector("#task-list");
     list.appendChild(newTask);
 
-    //adicionar o evento de remover
-    const removeBtn = newTask.querySelector(".remove-btn").addEventListener("click", function() {
-        removeTask(this);
+    // Adicionar o evento de remover
+    const removeBtn = newTask
+      .querySelector(".remove-btn")
+      .addEventListener("click", function () {
+        removeTask(this); // Refere-se à tarefa em si para saber qual elemento que quero remover
       });
 
-    //adicionar o evento de completar tarefa
-    const doneBtn = newTask.querySelector(".done-btn").addEventListener("click", function() {
+    // Adicionar evento de completar tarefa
+    const doneBtn = newTask
+      .querySelector(".done-btn")
+      .addEventListener("click", function () {
         completeTask(this)
-    });
+      });
 
-    //limpar texto
+    // Limpar texto
     document.querySelector("#task-title").value = "";
+  } else {
+    alert("Preencha a tarefa para prosseguir!");
   }
 }
 
-//Função que remove tarefa
+// Função de remover tarefa
 function removeTask(task) {
-  task.parentNode.remove(true);
+  task.parentNode.remove(true); // Acessa o elemento pai (li) e remove a tarefa
 }
 
-//Função que completar tarefa
+// Função de completar tarefa
 function completeTask(task) {
-    const taskComplete = task.parentNode;
-    taskComplete.classList.toggle("done"); //toggle serve para ver se tem a classe, se tiver coloca, se nao tiver, remove (subsititui o if else)
+  const taskComplete = task.parentNode
+  taskComplete.classList.toggle("done") // Se estiver com o done ele tira, se não estiver ele coloca, verifica se o elemento tem a classe ou não (substitui o if else)
 }
 
-//Evento de adicionar tarefa
+// Evento de adicionar tarega
 const addBtn = document.querySelector("#add-btn");
 addBtn.addEventListener("click", function (e) {
-  e.preventDefault();
+  e.preventDefault(); // Impede que o evento padrão ocorra - adiciona o form aqui no front-end e não no servidor
 
   addTask();
 });
